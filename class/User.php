a0645c2140b49c489ab8020437909106afd40527
mohsen <Mohsenverdizadehkohi@gmail.com>
2024-08-17 16:24:04 +0330

    move searchUser and addUser function from functions.php to User.php and convert them to static functions

--- class/User.php
+++ class/User.php
@@ -40,6 +40,23 @@ class User
         unset($this->weblogs[$weblog->getTitle()]);
     }
 
+    public  static function searchUser(string $userEmail)
+    {
+        $users = json_decode(file_get_contents('database/users.txt'), true);
+
+        if(array_key_exists($userEmail, $users)) {
+            return $users[$userEmail];
+        } else {
+            return null;
+        }
+    }
+
+    public static function addUser(User $user)
+    {
+        $users = json_decode(file_get_contents('database/users.txt'), true);
+        $users[$user->getEmail()] = $user;
+        file_put_contents('database/users.txt', json_encode($users));
+    }
     public function toArray()
     {
         return [
